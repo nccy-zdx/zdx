880479a38016507b4f5edec17df48aac5e784658
nccy-zdx <nccyzdx@163.com>
2025-03-17 17:33:04 +0800

    removed all from nccy.git

---
+++ lab1/hellonum.java
@@ -0,0 +1,9 @@
+public class hellonum {
+    public static int larger(int x,int y){
+        if(x>y) return x;
+        return y;
+    }
+    public static void main(String[] args){
+        System.out.println(larger(2,5));
+    }
+}
