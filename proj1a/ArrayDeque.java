80d6c849a7c07a3224077025242a90f5757a8650
nccy-zdx <nccyzdx@163.com>
2025-03-26 19:33:22 +0800

    proj1a fixed

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,13 +3,23 @@ public class ArrayDeque<T> {
     private int size;
     private int nextfirst;
     private int nextlast;
+    private double loadfactor;
 
     private void resize(int capacity){
         T[] arr=(T[]) new Object[capacity];
-        System.arraycopy(items, nextfirst+1, arr, arr.length+nextfirst-items.length, items.length-nextfirst-1);
-        System.arraycopy(items, 0, arr, 0, nextlast);
-        nextfirst=arr.length+nextfirst-items.length-1;
+        for(int i=0;i<nextlast;++i) arr[i]=items[i];
+        for(int i=0;i<items.length-nextfirst-1;++i){
+            arr[arr.length+nextfirst-items.length+i+1]=items[nextfirst+1+i];
+        }
+        //exception circumstance
+        if(nextfirst>nextlast&&items.length<=8){
+            for(int i=0;i<items.length;++i) arr[i]=items[i];
+            nextlast=nextfirst+1;
+            nextfirst=arr.length-1;
+        }
+        else nextfirst=arr.length+nextfirst-items.length;
         items=arr;
+        loadfactor=size/items.length;
     }
     
     public ArrayDeque(){
@@ -17,20 +27,25 @@ public class ArrayDeque<T> {
         items=(T []) new Object[8];
         nextfirst=4;
         nextlast=5;
+        loadfactor=size/items.length;
     }
 
     public void addFirst(T item){
         if(size==items.length) resize(size*2);
+        if(loadfactor<=0.25&&items.length>=16) resize(items.length/2);
         ++size;
         items[nextfirst]=item;
+        loadfactor=size/items.length;
         if(nextfirst>0) --nextfirst;
         else nextfirst=items.length-1;
     }
     
     public void addLast(T item){
         if(size==items.length) resize(size*2);
+        if(loadfactor<=0.25&&items.length>=16) resize(items.length/2);
         ++size;
         items[nextlast]=item;
+        loadfactor=size/items.length;
         if(nextlast<items.length-1) ++nextlast;
         else nextlast=0;
     }
@@ -51,25 +66,31 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst(){
-        if(size==0) return null;
-        ++nextfirst;
+        if(isEmpty()) return null;
+        if(nextfirst<items.length-1) ++nextfirst;
+        else nextfirst=0;
         --size;
         T item=items[nextfirst];
+        loadfactor=size/items.length;
+        if(loadfactor<=0.25&items.length>=16) resize(items.length/2);
         return item;
     }
 
     public T removeLast(){
-        if(size==0) return null;
-        --nextlast;
+        if(isEmpty()) return null;
+        if(nextlast>0) --nextlast;
+        else nextlast=items.length-1;
         --size;
         T item=items[nextlast];
+        loadfactor=size/items.length;
+        if(loadfactor<=0.25&items.length>=16) resize(items.length/2);
         return item;
     }
 
     public T get(int index){
-        if(index<0||index>size) return null;
-        if(nextfirst+index>=size){
-            return items[index-items.length+nextfirst];
+        if(index<0||index>size||isEmpty()) return null;
+        if(nextfirst+index>=items.length-1){
+            return items[index-items.length+nextfirst+1];
         }
         else{
             return items[nextfirst+index+1];
--- proj1a/test.java
+++ proj1a/test.java
@@ -9,25 +9,21 @@ public class test {
         lld.addFirst(0);
         lld.addLast(1);
         lld.addFirst(3);
-        int i=lld.getRecursive(1);
-        assertEquals(0, i);
+        for(int i=0;i<50;++i) lld.addFirst(i);
+        lld.removeFirst();
+        lld.removeLast();
+        int i=lld.getRecursive(0);
+        assertEquals(48, i);
     }
 
     @Test
     public void testforad(){
         ArrayDeque<Integer> ad=new ArrayDeque<>();
-        ad.addFirst(1);
-        ad.addLast(0);
         for(int i=0;i<10;++i) ad.addFirst(i);
-        assertEquals(12, ad.size());
-        int i=ad.get(0);
-        assertEquals(9,i);
-        int j=ad.removeFirst();
-        assertEquals(9, j);
-        int k=ad.get(9);
-        assertEquals(1, k);
-        int m=ad.removeLast();
-        assertEquals(0, m);
+        for(int i=0;i<8;++i) ad.removeLast();
+        int i=ad.removeLast();
+        assertEquals(3, i);
+
     }
 
 }
