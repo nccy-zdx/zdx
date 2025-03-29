f37111d1ead99df3aa5ac9b4de1999b8efa339d1
nccy-zdx <nccyzdx@163.com>
2025-03-29 12:22:50 +0800

    finished

---
+++ proj1gold/TextArrayDequeGold.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TextArrayDequeGold {
+
+    String message;
+
+    @Test
+    public void test(){
+        StudentArrayDeque<Integer> sad=new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads=new ArrayDequeSolution<>();
+        for(Integer i=-100;i<100;++i){
+            double randomnumber= StdRandom.uniform(-1.0,1.0);
+            if(randomnumber<-0.5){
+                message+="addFirst("+i+")\n";
+                sad.addFirst(i);
+                ads.addFirst(i);
+                assertEquals(message,sad.size(), ads.size());
+            }
+            if(randomnumber>0.5){
+                message+="addLast("+i+")\n";
+                sad.addLast(i);
+                ads.addLast(i);
+                assertEquals(message,ads.size(), sad.size());
+            }
+            if(randomnumber>=0&&randomnumber<=0.5){
+                if(sad.size()==0) continue;
+                Integer x=sad.removeFirst();
+                Integer y=ads.removeFirst();
+                message+="removeFirst():"+x+"\n";
+                assertEquals(message,x, y);
+            }
+            if(randomnumber<0&&randomnumber>=-0.5){
+                if(sad.size()==0) continue;
+                Integer x=sad.removeLast();
+                Integer y=ads.removeLast();
+                message+="removeLast():"+x+"\n";
+                assertEquals(message,x, y);
+            }
+        }
+    }
+
+}
