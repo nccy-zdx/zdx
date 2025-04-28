18079919fc44574c89c0660198239d80a99ce9ae
nccy-zdx <nccyzdx@163.com>
2025-04-28 18:15:05 +0800

    I forget the times

--- hw2/hw2/PercolationVisualizer.java
+++ hw2/hw2/PercolationVisualizer.java
@@ -89,7 +89,7 @@ public class PercolationVisualizer {
     }
 
     public static void main(String[] args) {
-        String filename = "inputFiles/input20.txt";
+        String filename = "inputFiles/input10-no.txt";
         simulateFromFile(filename);
     }
 }
