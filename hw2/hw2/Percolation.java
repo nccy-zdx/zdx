74b7d6423cd7c8867086245c86214e96fc4bfca1
nccy-zdx <nccyzdx@163.com>
2025-04-30 17:09:40 +0800

    Can it pass the AG?

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private WeightedQuickUnionUF set;
+    private WeightedQuickUnionUF setbelow;
     private int[][] data; //record opendata with 1
     private int count;   //record the number of open
     private final int Length; //N-1
@@ -15,7 +16,8 @@ public class Percolation {
             throw e;
         }
         data=new int[N][N];
-        set=new WeightedQuickUnionUF(N*N+1);
+        set=new WeightedQuickUnionUF(N*N+2);
+        setbelow=new WeightedQuickUnionUF(N*N+1);
         Length=N-1;
         count=0;
         for(int i=0;i<N;++i){
@@ -37,14 +39,27 @@ public class Percolation {
         if(data[row][col]==1) return;
         data[row][col]=1;
         ++count;
-        if(col!=Length&&data[row][col+1]!=0) set.union(row*data[0].length+col, row*data[0].length+col+1); //right
-        if(col!=0&&data[row][col-1]!=0) set.union(row*data[0].length+col, row*data[0].length+col-1); //left
-        if(row!=0&&data[row-1][col]!=0) set.union(row*data[0].length+col, (row-1)*data[0].length+col); //upper
-        else if(row==0) set.union(col, (Length+1)*(Length+1));
-        if(row!=Length&&data[row+1][col]!=0) set.union(row*data[0].length+col, (row+1)*data[0].length+col); //down
-        else if(row==Length){
-
+        if(col!=Length&&data[row][col+1]!=0){
+            set.union(row*data[0].length+col, row*data[0].length+col+1); 
+            setbelow.union(row*data[0].length+col, row*data[0].length+col+1); 
+        }//right
+        if(col!=0&&data[row][col-1]!=0){
+            set.union(row*data[0].length+col, row*data[0].length+col-1); 
+            setbelow.union(row*data[0].length+col, row*data[0].length+col-1); 
+        } //left
+        if(row!=0&&data[row-1][col]!=0){
+            set.union(row*data[0].length+col, (row-1)*data[0].length+col); 
+            setbelow.union(row*data[0].length+col, (row-1)*data[0].length+col); 
+        }//upper
+        else if(row==0){
+            set.union(col, (Length+1)*(Length+1));
+            setbelow.union(col, (Length+1)*(Length+1));
         }
+        if(row!=Length&&data[row+1][col]!=0){
+            set.union(row*data[0].length+col, (row+1)*data[0].length+col); 
+            setbelow.union(row*data[0].length+col, (row+1)*data[0].length+col); 
+        }//down
+        else if(row==Length) set.union(row*data[0].length+col, (Length+1)*(Length+1)+1);
     }
 
     //check if site is open. O(1).
@@ -56,7 +71,8 @@ public class Percolation {
     //check if it`s near other opens. If it is. Union them and check if a open site is a full site. O(N).Now O(1).
     public boolean isFull(int row,int col){
         checkException(row, col);
-        return set.connected((Length+1)*(Length+1), row*data[0].length+col);
+        if(data[row][col]!=1) return false;
+        return setbelow.connected((Length+1)*(Length+1), row*data[0].length+col)&&set.connected((Length+1)*(Length+1), row*data[0].length+col);
     }
 
     //return number of open sites whether it`s a full site or not. O(1).
@@ -66,17 +82,20 @@ public class Percolation {
 
     //check if percolates. O(N^2). Needed to be improved to at least O(N).Now O(1).Now O(N)....
     public boolean percolates(){
-        int id=set.find((Length+1)*(Length+1));
-        int idbottom;
-        for(int i=0;i<data[0].length;++i){
-            if(data[Length][i]==1){
-                idbottom=set.find(Length*data[0].length+i);
-                if(id==idbottom) return true;
-            }
-        }
-        return false;
+        return set.connected((Length+1)*(Length+1), (Length+1)*(Length+1)+1);
     }
 
     public static void main(String[] args) { //just for test. 
-    }
+        int N=5;
+        Percolation per=new Percolation(N);
+        for(int i=0;i<N-3;++i){
+            per.open(i, 0);
+        }
+        per.open(3,0);
+        per.open(4, 0);
+        per.open(4, 2);
+        per.open(3, 2);
+        per.open(2, 0);
+        System.out.println(per.isFull(3, 2));
+    }    
 }
