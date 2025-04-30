eec76a44f36ab00832c948686a2012f9a4f814dc
nccy-zdx <nccyzdx@163.com>
2025-04-30 17:54:11 +0800

    HW2 Percolation passed. I don	 know how many times I have commited oh no

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,12 +3,13 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private WeightedQuickUnionUF set;
+    private WeightedQuickUnionUF setabove;
     private WeightedQuickUnionUF setbelow;
     private int[][] data; //record opendata with 1
     private int count;   //record the number of open
     private final int Length; //N-1
     
+    //Sacrfice space will upgrade runtime a lot...
     //O(N^2), 0 presents that it`s blocked, while 1 means opened. Initialize it with blocked.
     public Percolation(int N){
         if(N<=0){
@@ -16,7 +17,7 @@ public class Percolation {
             throw e;
         }
         data=new int[N][N];
-        set=new WeightedQuickUnionUF(N*N+2);
+        setabove=new WeightedQuickUnionUF(N*N+2);
         setbelow=new WeightedQuickUnionUF(N*N+1);
         Length=N-1;
         count=0;
@@ -25,7 +26,7 @@ public class Percolation {
         }
     }
 
-    //helper method to save space.
+    //helper method to save space and enhance readablity.
     private void checkException(int row, int col){
         if(row>Length||col>Length||row<0||col<0){
             IndexOutOfBoundsException e=new IndexOutOfBoundsException();
@@ -33,33 +34,24 @@ public class Percolation {
         }
     } 
 
+    //helper method to save space and increase readablity.
+    private void union(int row,int col){
+        setabove.union(row, col);
+        setbelow.union(row, col);
+    }
+
     //Use disjointset,fast. O(1).
     public void open(int row,int col){
         checkException(row, col);
         if(data[row][col]==1) return;
         data[row][col]=1;
         ++count;
-        if(col!=Length&&data[row][col+1]!=0){
-            set.union(row*data[0].length+col, row*data[0].length+col+1); 
-            setbelow.union(row*data[0].length+col, row*data[0].length+col+1); 
-        }//right
-        if(col!=0&&data[row][col-1]!=0){
-            set.union(row*data[0].length+col, row*data[0].length+col-1); 
-            setbelow.union(row*data[0].length+col, row*data[0].length+col-1); 
-        } //left
-        if(row!=0&&data[row-1][col]!=0){
-            set.union(row*data[0].length+col, (row-1)*data[0].length+col); 
-            setbelow.union(row*data[0].length+col, (row-1)*data[0].length+col); 
-        }//upper
-        else if(row==0){
-            set.union(col, (Length+1)*(Length+1));
-            setbelow.union(col, (Length+1)*(Length+1));
-        }
-        if(row!=Length&&data[row+1][col]!=0){
-            set.union(row*data[0].length+col, (row+1)*data[0].length+col); 
-            setbelow.union(row*data[0].length+col, (row+1)*data[0].length+col); 
-        }//down
-        else if(row==Length) set.union(row*data[0].length+col, (Length+1)*(Length+1)+1);
+        if(col!=Length&&data[row][col+1]!=0) union(row*data[0].length+col, row*data[0].length+col+1); //right
+        if(col!=0&&data[row][col-1]!=0) union(row*data[0].length+col, row*data[0].length+col-1); //left
+        if(row!=0&&data[row-1][col]!=0) union(row*data[0].length+col, (row-1)*data[0].length+col); //upper
+        else if(row==0) union(col, (Length+1)*(Length+1));
+        if(row!=Length&&data[row+1][col]!=0) union(row*data[0].length+col, (row+1)*data[0].length+col); //down
+        else if(row==Length) setabove.union(row*data[0].length+col, (Length+1)*(Length+1)+1);
     }
 
     //check if site is open. O(1).
@@ -72,7 +64,8 @@ public class Percolation {
     public boolean isFull(int row,int col){
         checkException(row, col);
         if(data[row][col]!=1) return false;
-        return setbelow.connected((Length+1)*(Length+1), row*data[0].length+col)&&set.connected((Length+1)*(Length+1), row*data[0].length+col);
+        return setbelow.connected((Length+1)*(Length+1), row*data[0].length+col)&&
+        setabove.connected((Length+1)*(Length+1), row*data[0].length+col);
     }
 
     //return number of open sites whether it`s a full site or not. O(1).
@@ -80,22 +73,11 @@ public class Percolation {
         return count;
     }
 
-    //check if percolates. O(N^2). Needed to be improved to at least O(N).Now O(1).Now O(N)....
+    //check if percolates. O(N^2). Needed to be improved to at least O(N).Now O(1).Now O(N)....,Now O(1). All O(1).wuwu
     public boolean percolates(){
-        return set.connected((Length+1)*(Length+1), (Length+1)*(Length+1)+1);
+        return setabove.connected((Length+1)*(Length+1), (Length+1)*(Length+1)+1);
     }
 
     public static void main(String[] args) { //just for test. 
-        int N=5;
-        Percolation per=new Percolation(N);
-        for(int i=0;i<N-3;++i){
-            per.open(i, 0);
-        }
-        per.open(3,0);
-        per.open(4, 0);
-        per.open(4, 2);
-        per.open(3, 2);
-        per.open(2, 0);
-        System.out.println(per.isFull(3, 2));
     }    
 }
