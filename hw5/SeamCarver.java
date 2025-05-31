8400a5edbb40d9b08d5c4a9c70b3954e1370f5d5
nccy-zdx <nccyzdx@163.com>
2025-05-31 17:15:49 +0800

    nengyong

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -3,16 +3,59 @@ import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
 
-import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
     private Picture picture;
-    private double[][] previouss;
+    private double[][] energies;
+    private Picture transPicture;
+    private boolean isHorizonal=false;
+    private int[] seam;
+    private double minEnergy;
 
     public SeamCarver(Picture picture){
         this.picture=new Picture(picture);
-        previouss=new double[picture.height()][picture.width()];
+        minEnergy=energy(0, 0);
+    }
+
+    private void settrans(){
+        transPicture=new Picture(picture.height(), picture.width());
+        for(int i=0;i<picture.width();++i){
+            for(int j=0;j<picture.height();++j){
+                transPicture.set(j, i, picture.get(i, j));
+            }
+        }
+    }
+
+    private void setEnergies(int[] seam){
+        if(seam==null){
+            energies=new double[picture.height()][picture.width()];
+            for(int i=0;i<picture.width();++i){
+                for(int j=0;j<picture.height();++j){
+                    energies[j][i]=energy(i, j);
+                    if(minEnergy>energies[j][i]) minEnergy=energies[j][i];
+                }
+            }
+        }
+        else{
+            for(int i=0;i<seam.length;++i){
+                if(seam[i]==0){
+                    energies[i][0]=energy(0, i);
+                    energies[i][picture.width()-1]=energy(picture.width()-1, i);
+                } 
+                else if(seam[i]==picture.width()){
+                    energies[i][picture.width()-1]=energy(picture.width()-1, i);
+                    energies[i][0]=energy(0, i); 
+                } 
+                else{
+                    energies[i][seam[i]-1]=energy(seam[i]-1, i);
+                    energies[i][seam[i]]=energy(seam[i], i);
+                }
+                for(int j=seam[i]+1;j<picture.width();++j){
+                    energies[i][j]=energies[i][j+1];
+                }
+            }
+        }
     }
     
     public Picture picture(){
@@ -29,7 +72,7 @@ public class SeamCarver {
 
     public double energy(int x,int y){
         if(x<0||x>=picture.width()||y<0||y>=picture.height()) throw new IndexOutOfBoundsException("col:"+x+"  row:"+y);
-        double rx,gx,bx,ry,gy,by,sumx,sumy;
+        double rx,gx,bx,ry,gy,by;
         Color front,back,upper,down;
 
         if(x==picture.width()-1) front=picture.get(0, y);
@@ -48,97 +91,92 @@ public class SeamCarver {
         gy=Math.abs(down.getGreen()-upper.getGreen());
         by=Math.abs(down.getBlue()-upper.getBlue());
 
-        sumx=rx*rx+gx*gx+bx*bx;
-        sumy=ry*ry+gy*gy+by*by;
-
-        return sumx+sumy;
+        return rx*rx+gx*gx+bx*bx+ry*ry+gy*gy+by*by;
     } 
 
     public int[] findHorizontalSeam(){
-        Picture transpicture=new Picture(picture.height(), picture.width());
-        for(int i=0;i<picture.width();++i){
-            for(int j=0;j<picture.height();++j){
-                transpicture.set(j, i, picture.get(i, j));
-            }
-        }
-        previouss=new double[picture.width()][picture.height()];
         Picture reservepic=new Picture(picture);
-        picture=transpicture;
+        isHorizonal=true;
+        settrans();
+        picture=transPicture;
         int[] shortpath=findVerticalSeam();
         picture=reservepic;
+        isHorizonal=false;
         return shortpath;
     }
 
     public int[] findVerticalSeam(){
+        Node minNode=null;
         int[] shortpath=new int[picture.height()];
-        MinPQ<Node> path=new MinPQ<>();
+        Set<Node> set=new HashSet<>();
         PriorityQueue<Node> pq=new PriorityQueue<>();
-        //int count=0;
+        double[][] previouss;
+        if(isHorizonal){
+            setEnergies(seam);
+            previouss=new double[transPicture.height()][transPicture.width()];
+        }
+        else{
+            setEnergies(seam);
+            previouss=new double[picture.height()][picture.width()];
+        }
         for(int i=0;i<picture.width();++i){
-            Node row=new Node(energy(i, 0), i, 0, 0, null);
+            Node row=new Node(i, 0, 0, null);
             pq.add(row);
         }//W
-        Set<Node> set=new HashSet<>();
+
         while(!pq.isEmpty()){
             Node bsm=pq.remove();
             previouss[bsm.rownum][bsm.colnum]=bsm.previous;
-            if(set.contains(bsm)) continue;
-            else if(!set.contains(bsm)&&bsm.rownum!=picture.height()-1) set.add(bsm);
-            //++count;
+            if(!set.contains(bsm)) set.add(bsm);
             if(bsm.rownum==picture.height()-1){
-                path.insert(bsm);
+                minNode=bsm;
                 break;
             }
             else if(bsm.colnum==0){
-                Node n1=new Node(energy(bsm.colnum, bsm.rownum+1), bsm.colnum, bsm.rownum+1,bsm.previous,bsm);
-                Node n2=new Node(energy(bsm.colnum+1, bsm.rownum+1), bsm.colnum+1, bsm.rownum+1,bsm.previous,bsm);
-                checkAndAdd(n1, pq, bsm, set);
-                checkAndAdd(n2, pq, bsm, set);
+                Node n1=new Node(bsm.colnum, bsm.rownum+1,bsm.previous,bsm);
+                Node n2=new Node(bsm.colnum+1, bsm.rownum+1,bsm.previous,bsm);
+                checkAndAdd(n1, pq, bsm, set, previouss);
+                checkAndAdd(n2, pq, bsm, set, previouss);
             }
             else if(bsm.colnum==picture.width()-1){
-                Node n1=new Node(energy(bsm.colnum-1, bsm.rownum+1), bsm.colnum-1, bsm.rownum+1,bsm.previous,bsm);
-                Node n2=new Node(energy(bsm.colnum, bsm.rownum+1), bsm.colnum, bsm.rownum+1,bsm.previous,bsm);
-                checkAndAdd(n1, pq, bsm, set);
-                checkAndAdd(n2, pq, bsm, set);
+                Node n1=new Node(bsm.colnum-1, bsm.rownum+1,bsm.previous,bsm);
+                Node n2=new Node(bsm.colnum, bsm.rownum+1,bsm.previous,bsm);
+                checkAndAdd(n1, pq, bsm, set, previouss);
+                checkAndAdd(n2, pq, bsm, set, previouss);
             }
             else{
-                Node n1=new Node(energy(bsm.colnum-1, bsm.rownum+1), bsm.colnum-1, bsm.rownum+1,bsm.previous,bsm);
-                Node n2=new Node(energy(bsm.colnum, bsm.rownum+1), bsm.colnum, bsm.rownum+1,bsm.previous,bsm);
-                Node n3=new Node(energy(bsm.colnum+1, bsm.rownum+1), bsm.colnum+1, bsm.rownum+1,bsm.previous,bsm);
-                checkAndAdd(n1, pq, bsm, set);
-                checkAndAdd(n2, pq, bsm, set);
-                checkAndAdd(n3, pq, bsm, set);
+                Node n1=new Node(bsm.colnum-1, bsm.rownum+1,bsm.previous,bsm);
+                Node n2=new Node(bsm.colnum, bsm.rownum+1,bsm.previous,bsm);
+                Node n3=new Node(bsm.colnum+1, bsm.rownum+1,bsm.previous,bsm);
+                checkAndAdd(n1, pq, bsm, set, previouss);
+                checkAndAdd(n2, pq, bsm, set, previouss);
+                checkAndAdd(n3, pq, bsm, set, previouss);
             }
         }//W*H
-        //System.out.println(count);
 
-        Node minNode=path.delMin();
         while(minNode.rownum!=0){
             shortpath[minNode.rownum]=minNode.colnum;
             minNode=minNode.preNode;
         }//H
-
         shortpath[0]=minNode.colnum;
+
         return shortpath;
     }
 
-    private double h(Node n){
-        return 0;
-    }
-
-    private void checkAndAdd(Node n,PriorityQueue<Node> pq,Node bsm,Set<Node> set){
-        if(pq.contains(n)&&bsm.previous<minUpperPrevious(n)){
+    private void checkAndAdd(Node n,PriorityQueue<Node> pq,Node bsm,Set<Node> set,double[][] previouss){
+        if(set.contains(n)) return;
+        if(pq.contains(n)&&bsm.previous<minUpperPrevious(n,previouss)){
             pq.remove(n);
             pq.add(n);
             previouss[n.rownum][n.colnum]=n.previous;
         }
-        else if(!pq.contains(n)&&!set.contains(n)){
+        else if(!pq.contains(n)){
             pq.add(n);
             previouss[n.rownum][n.colnum]=n.previous;
         }
     }
 
-    private double minUpperPrevious(Node n){
+    private double minUpperPrevious(Node n,double[][] previouss){
         if(n.colnum==0){
             double energy1=previouss[n.rownum-1][0];
             double energy2=previouss[n.rownum-1][1];
@@ -189,25 +227,22 @@ public class SeamCarver {
         public double previous;
         public Node preNode;
 
-        public Node(double energy,int colnum,int rownum,double previous,Node pre){
+        public Node(int colnum,int rownum,double previous,Node pre){
             this.colnum=colnum;
             this.rownum=rownum;
-            this.previous=previous+energy;
+            this.previous=previous+energies[rownum][colnum];
             this.preNode=pre;
         }
 
         @Override
-        public int compareTo(Node n){        
-            if(n.previous+h(n)>previous+h(this)) return -1;
-            else if(n.previous+h(n)<previous+h(this)) return 1;
+        public int compareTo(Node n){      
+            if(n.previous+(picture.height()-1-n.rownum)*minEnergy>previous+(picture.height()-1-rownum)*minEnergy) return -1;
+            else if(n.previous+(picture.height()-1-n.rownum)*minEnergy<previous+(picture.height()-1-rownum)*minEnergy) return 1;
             else return 0;
         }
 
         @Override
         public boolean equals(Object obj) {
-            if(obj==null) return false;
-            if(obj==this) return true;
-            if(obj.getClass()!=this.getClass()) return false;
             Node n=((Node)obj);
             if(n.rownum==rownum&&n.colnum==colnum) return true;
             else return false;
@@ -215,19 +250,21 @@ public class SeamCarver {
 
         @Override
         public int hashCode() {
-            return colnum<<31+rownum;
-        }
+            return (rownum+53)*53+colnum;
+        }//哈希啊哈希，你让我好找又好想啊。
         
     }
 
     public void removeHorizontalSeam(int[] seam){
         if(seam.length!=picture.width()) throw new IllegalArgumentException();
-        SeamRemover.removeHorizontalSeam(picture, seam);
+        picture=SeamRemover.removeHorizontalSeam(picture, seam);
+        this.seam=seam;
     }
 
     public void removeVerticalSeam(int[] seam){
         if(seam.length!=picture.height()) throw new IllegalArgumentException();
-        SeamRemover.removeVerticalSeam(picture, seam);
+        picture=SeamRemover.removeVerticalSeam(picture, seam);
+        this.seam=seam;
     }
 
 }
